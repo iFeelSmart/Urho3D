d0d085ab262272a8a6e795452b72f30f60489fa2
Nicolas Choiset <nicolas.choiset@ifeelsmart.com>
2017-10-02 13:57:32 +0200

    Add clear all keyframes added for animations

--- Source/Urho3D/Scene/ValueAnimation.h
+++ Source/Urho3D/Scene/ValueAnimation.h
@@ -34,136 +34,139 @@ class JSONValue;
 /// Interpolation method.
 enum InterpMethod
 {
-    /// No interpolation.
-    IM_NONE = 0,
-    /// Linear interpolation (default).
-    IM_LINEAR,
-    /// Cardinal spline interpolation, default tension value is 0.5f. For more information please refer to http://cubic.org/docs/hermite.htm.
-    IM_SPLINE,
+	/// No interpolation.
+	IM_NONE = 0,
+	/// Linear interpolation (default).
+	IM_LINEAR,
+	/// Cardinal spline interpolation, default tension value is 0.5f. For more information please refer to http://cubic.org/docs/hermite.htm.
+	IM_SPLINE,
 };
 
 /// Value animation key frame.
 struct VAnimKeyFrame
 {
-    /// Time.
-    float time_;
-    /// Value.
-    Variant value_;
+	/// Time.
+	float time_;
+	/// Value.
+	Variant value_;
 };
 
 /// Value animation event frame.
 struct VAnimEventFrame
 {
-    /// Time.
-    float time_;
-    /// Event type.
-    StringHash eventType_;
-    /// Event data.
-    VariantMap eventData_;
+	/// Time.
+	float time_;
+	/// Event type.
+	StringHash eventType_;
+	/// Event data.
+	VariantMap eventData_;
 };
 
 /// Value animation class.
 class URHO3D_API ValueAnimation : public Resource
 {
-    URHO3D_OBJECT(ValueAnimation, Resource);
+	URHO3D_OBJECT(ValueAnimation, Resource);
 
 public:
-    /// Construct.
-    ValueAnimation(Context* context);
-    /// Destruct.
-    virtual ~ValueAnimation() override;
-    /// Register object factory.
-    static void RegisterObject(Context* context);
+	/// Construct.
+	ValueAnimation(Context* context);
+	/// Destruct.
+	virtual ~ValueAnimation() override;
+	/// Register object factory.
+	static void RegisterObject(Context* context);
 
-    /// Load resource from stream. May be called from a worker thread. Return true if successful.
-    virtual bool BeginLoad(Deserializer& source) override;
-    /// Save resource. Return true if successful.
-    virtual bool Save(Serializer& dest) const override;
-    /// Load from XML data. Return true if successful.
-    bool LoadXML(const XMLElement& source);
-    /// Save as XML data. Return true if successful.
-    bool SaveXML(XMLElement& dest) const;
-    /// Load from JSON data. Return true if successful.
-    bool LoadJSON(const JSONValue& source);
-    /// Save as XML data. Return true if successful.
-    bool SaveJSON(JSONValue& dest) const;
+	/// Load resource from stream. May be called from a worker thread. Return true if successful.
+	virtual bool BeginLoad(Deserializer& source) override;
+	/// Save resource. Return true if successful.
+	virtual bool Save(Serializer& dest) const override;
+	/// Load from XML data. Return true if successful.
+	bool LoadXML(const XMLElement& source);
+	/// Save as XML data. Return true if successful.
+	bool SaveXML(XMLElement& dest) const;
+	/// Load from JSON data. Return true if successful.
+	bool LoadJSON(const JSONValue& source);
+	/// Save as XML data. Return true if successful.
+	bool SaveJSON(JSONValue& dest) const;
 
-    /// Set owner.
-    void SetOwner(void* owner);
-    /// Set interpolation method.
-    void SetInterpolationMethod(InterpMethod method);
-    /// Set spline tension, should be between 0.0f and 1.0f, but this is not a must.
-    void SetSplineTension(float tension);
-    /// Set value type.
-    void SetValueType(VariantType valueType);
+	/// Set owner.
+	void SetOwner(void* owner);
+	/// Set interpolation method.
+	void SetInterpolationMethod(InterpMethod method);
+	/// Set spline tension, should be between 0.0f and 1.0f, but this is not a must.
+	void SetSplineTension(float tension);
+	/// Set value type.
+	void SetValueType(VariantType valueType);
 
-    /// Set key frame.
-    bool SetKeyFrame(float time, const Variant& value);
-    /// Set event frame.
-    void SetEventFrame(float time, const StringHash& eventType, const VariantMap& eventData = VariantMap());
+	/// Clear all keys
+	void ClearKeyFrames();
 
-    /// Return animation is valid.
-    bool IsValid() const;
+	/// Set key frame.
+	bool SetKeyFrame(float time, const Variant& value);
+	/// Set event frame.
+	void SetEventFrame(float time, const StringHash& eventType, const VariantMap& eventData = VariantMap());
 
-    /// Return owner.
-    void* GetOwner() const { return owner_; }
+	/// Return animation is valid.
+	bool IsValid() const;
 
-    /// Return interpolation method.
-    InterpMethod GetInterpolationMethod() const { return interpolationMethod_; }
+	/// Return owner.
+	void* GetOwner() const { return owner_; }
 
-    /// Return spline tension.
-    float GetSplineTension() const { return splineTension_; }
+	/// Return interpolation method.
+	InterpMethod GetInterpolationMethod() const { return interpolationMethod_; }
 
-    /// Return value type.
-    VariantType GetValueType() const { return valueType_; }
+	/// Return spline tension.
+	float GetSplineTension() const { return splineTension_; }
 
-    /// Return begin time.
-    float GetBeginTime() const { return beginTime_; }
+	/// Return value type.
+	VariantType GetValueType() const { return valueType_; }
 
-    /// Return end time.
-    float GetEndTime() const { return endTime_; }
+	/// Return begin time.
+	float GetBeginTime() const { return beginTime_; }
 
-    /// Return animation value.
-    Variant GetAnimationValue(float scaledTime);
+	/// Return end time.
+	float GetEndTime() const { return endTime_; }
 
-    /// Has event frames.
-    bool HasEventFrames() const { return !eventFrames_.Empty(); }
+	/// Return animation value.
+	Variant GetAnimationValue(float scaledTime);
 
-    /// Return all event frames between time.
-    void GetEventFrames(float beginTime, float endTime, PODVector<const VAnimEventFrame*>& eventFrames) const;
+	/// Has event frames.
+	bool HasEventFrames() const { return !eventFrames_.Empty(); }
+
+	/// Return all event frames between time.
+	void GetEventFrames(float beginTime, float endTime, PODVector<const VAnimEventFrame*>& eventFrames) const;
 
 protected:
-    /// Linear interpolation.
-    Variant LinearInterpolation(unsigned index1, unsigned index2, float scaledTime) const;
-    /// Spline interpolation.
-    Variant SplineInterpolation(unsigned index1, unsigned index2, float scaledTime);
-    /// Update spline tangents.
-    void UpdateSplineTangents();
-    /// Return (value1 - value2) * t.
-    Variant SubstractAndMultiply(const Variant& value1, const Variant& value2, float t) const;
+	/// Linear interpolation.
+	Variant LinearInterpolation(unsigned index1, unsigned index2, float scaledTime) const;
+	/// Spline interpolation.
+	Variant SplineInterpolation(unsigned index1, unsigned index2, float scaledTime);
+	/// Update spline tangents.
+	void UpdateSplineTangents();
+	/// Return (value1 - value2) * t.
+	Variant SubstractAndMultiply(const Variant& value1, const Variant& value2, float t) const;
 
-    /// Owner.
-    void* owner_;
-    /// Interpolation method.
-    InterpMethod interpolationMethod_;
-    /// Spline tension.
-    float splineTension_;
-    /// Value type.
-    VariantType valueType_;
-    /// Interpolatable flag.
-    bool interpolatable_;
-    /// Begin time.
-    float beginTime_;
-    /// End time.
-    float endTime_;
-    /// Key frames.
-    Vector<VAnimKeyFrame> keyFrames_;
-    /// Spline tangents.
-    VariantVector splineTangents_;
-    /// Spline tangents dirty.
-    bool splineTangentsDirty_;
-    /// Event frames.
-    Vector<VAnimEventFrame> eventFrames_;
+	/// Owner.
+	void* owner_;
+	/// Interpolation method.
+	InterpMethod interpolationMethod_;
+	/// Spline tension.
+	float splineTension_;
+	/// Value type.
+	VariantType valueType_;
+	/// Interpolatable flag.
+	bool interpolatable_;
+	/// Begin time.
+	float beginTime_;
+	/// End time.
+	float endTime_;
+	/// Key frames.
+	Vector<VAnimKeyFrame> keyFrames_;
+	/// Spline tangents.
+	VariantVector splineTangents_;
+	/// Spline tangents dirty.
+	bool splineTangentsDirty_;
+	/// Event frames.
+	Vector<VAnimEventFrame> eventFrames_;
 };
 
 }
